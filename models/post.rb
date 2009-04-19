96a9563e14630db12c10e4eddb1ca389b45cf460
Kosuke Matsuda <kosukematsuda@gmail.com>
2009-04-20 00:49:55 +0900

    add some view file and layout, modify routing, and modify model

--- models/post.rb
+++ models/post.rb
@@ -1,2 +1,19 @@
 class Post < Sequel::Model
+  def before_create
+    self.created_at = Time.now
+    self.updated_at = Time.now
+  end
+
+  def before_save
+    self.updated_at = Time.now
+  end
+
+  def validate
+    errors[:title] << "can't be empty" if title.empty?
+    errors[:body] << "can't be empty" if body.empty? || body.split('').length > 1000
+  end
+
+  def format_body
+    self.body && self.body.gsub(/\r\n/, '\n').gsub(/\n/, '<br />')
+  end
 end
--- potage.rb
+++ potage.rb
@@ -1,35 +1,85 @@
+__DIR__ = File.dirname(__FILE__)
+
 require 'rubygems'
 require 'sinatra'
 require 'sequel'
 
+# 
+# Configuration
+# 
 configure do
   DB = Sequel.connect('sqlite://db/potage.db')
-  p DB
-  DB.drop_table(:posts) if DB
-  DB.create_table :posts do
-    primary_key :id
-    String    :title
-    String    :body
-    DateTime  :created_at
-    DateTime  :updated_at
+  unless DB.table_exists?(:posts)
+    DB.create_table :posts do
+      primary_key :id
+      String    :title
+      String    :body
+      DateTime  :created_at
+      DateTime  :updated_at
+    end
   end
 end
 
-$LOAD_PATH.unshift(File.dirname(__FILE__) + '/models')
-require 'post'
+def require_or_load(file)
+  development? ? load(file) : require(file)
+end
 
+__MODEL_DIR__ = 'models'
+# $LOAD_PATH.unshift(File.dirname(__FILE__) + '/models')
+$LOAD_PATH.unshift(File.join(__DIR__, __MODEL_DIR__))
+require_or_load File.join(__MODEL_DIR__, 'post.rb')
+
+# 
+# Helpers
+# 
+helpers do
+  def date_format(date)
+    date && date.strftime("%Y/%m/%d %H:%M")
+  end
+  
+  def authorized?
+    true
+    # halt 401, 'Not Autorized'
+  end
+end
+
+# 
+# Routes
+# 
 get '/' do
-  @posts = Post.all
+  @post = Post.reverse_order(:updated_at).first
+  erb :index
 end
 
 get '/posts' do
   @posts = Post.reverse_order(:updated_at)
+  erb :posts
 end
 
 get '/post/:id' do
-  post = Post[params[:id]]
+  @post = Post[params[:id]]
+  erb :post
 end
 
-post '/post' do
-  post = Post.create(params[:post])
+get '/posts/new' do
+  authorized?
+  post = Post.new
+  erb :edit, :locals => {:post => post}
+end
+
+post '/posts' do
+  authorized?
+  post = Post.new :title => params[:title], :body => params[:body]
+  begin
+    post.save
+    redirect "/post/#{post.id}"
+  rescue Sequel::ValidationFailed => e
+    erb :edit, :locals => {:post => post}
+  end
+end
+
+get '/feeds' do
+  authorized?
+  content_type 'application/xml', :charset => 'utf-8'
+  builder :index
 end
